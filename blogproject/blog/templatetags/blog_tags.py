a55f3c1cefda895cab1141e3d84fc3d04110e4c9
wyt <353981086@qq.com>
2017-09-17 21:00:17 +0800

    ver 1.0

--- blogproject/blog/templatetags/blog_tags.py
+++ blogproject/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models.aggregates import Count
 from ..models import Post, Category, Tag
 
 
@@ -7,7 +8,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_recent_posts(num=5):
-    return Post.objects.all().order_by('-created_time')[:num]
+    return Post.objects.all()[:num]
 
 
 @register.simple_tag
@@ -17,4 +18,9 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,45 +1,66 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from comments.forms import CommentForm
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 3
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    # 增加阅读量
-    post.increase_views()
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
 
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {'post': post,
-               'form': form,
-               'comment_list': comment_list,
-               }
-    return render(request, 'blog/detail.html', context=context)
+        self.object.increase_views()
+
+        return response
+
+    def get_object(self, queryset=None):
+
+        post = super(PostDetailView, self).get_object(queryset=None)
+
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
+
+class ArchivesView(IndexView):
+
+    def get_queryset(self):
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'), created_time__month=self.kwargs.get('month'))
 
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month,
-                                    )
-    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-def category(request, pk):
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class TagView(IndexView):
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
